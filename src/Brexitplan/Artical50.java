6c79ddd796cb606ac6d4296bb8af68d75b20ceb3
bpatel25022012 <bpatel2502012@gmail.com>
2020-02-11 22:58:36 +0000

    my practice

---
+++ src/Brexitplan/Artical50.java
@@ -0,0 +1,15 @@
+package Brexitplan;
+
+public class Artical50 {
+    public static void main (String [] args){
+        System.out.println("Article 50");
+       System.out.println("Brexit happend on 31st of January 2020.");
+       System.out.println("Theresa May resigned as she could not Brexit done.");
+       System.out.println("Three Prim Ministered Changed in the span of 3 years.");
+       System.out.println("After too much hassles , we are now out of Europe now.");
+       System.out.println("Thank you Article 50, we are now independent");
+        System.out.println("");
+       System.out.println("Jai Hind");
+
+    }
+}
---
+++ src/Maths50.java
@@ -0,0 +1,56 @@
+public class Maths50 {
+    public static void main(String [] args){
+
+
+    double a = 2.5;
+    int b = 30;
+    double c = 10.57;
+
+    System.out.println(a+b-c);
+    System.out.println( a + " X " + b + " = " + a*b +"");
+    System.out.println( (a + b) + " X "+ c + " = " + (a+b)*c+"");
+    System.out.println((a+c) + "/" + b + " = " + (a+c)/b+"");
+
+        System.out.println("");
+    System.out.println((a + " X " + 1 + " = " + a*1 ));
+        System.out.println((a + " X " + 2 + " = " + a*2));
+        System.out.println((a + " X " + 3 + " = " + a*3 ));
+        System.out.println((a + " X " + 4 + " = " + a*4 ));
+        System.out.println((a + " X " + 5 + " = " + a*5 ));
+        System.out.println((a + " X " + 6 + " = " + a*6 ));
+        System.out.println((a + " X " + 7 + " = " + a*7 ));
+        System.out.println((a + " X " + 8 + " = " + a*8  ));
+        System.out.println((a + " X " + 9 + " = " + a*9  ));
+        System.out.println((a + " X " + 10 + " = " + a*10 ));
+
+        System.out.println("");
+        System.out.println("(25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)" + " = " + (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+
+        System.out.println("");
+        System.out.println("7 6 5 4 3 2 1 ");
+        System.out.println("7 6 5 4 3 2  ");
+        System.out.println("7 6 5 4 3  ");
+        System.out.println("7 6 5 4  ");
+        System.out.println("7 6 5  ");
+        System.out.println("7 6  ");
+        System.out.println("7 ");
+
+        System.out.println("");
+        System.out.println("1 2 3 4 5 6 7");
+        System.out.println(" 2 3 4 5 6 7");
+        System.out.println("  3 4 5 6 7");
+        System.out.println("   4 5 6 7");
+        System.out.println("    5 6 7");
+        System.out.println("     6 7");
+        System.out.println("      7");
+        System.out.println("     6 7");
+        System.out.println("    5 6 7");
+        System.out.println("   4 5 6 7");
+        System.out.println("  3 4 5 6 7");
+        System.out.println(" 2 3 4 5 6 7");
+        System.out.println("1 2 3 4 5 6 7");
+
+
+
+    }
+}
